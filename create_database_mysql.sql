bae8375dee6dd009da8fe8163840cb58ddb14d2b
Jatofi <56402844+jatofi-official@users.noreply.github.com>
2026-03-23 15:28:00 +0100

    Updated database

--- create_database_mysql.sql
+++ create_database_mysql.sql
@@ -4,16 +4,21 @@ USE tag_tracker;
 
 CREATE TABLE tags(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name TEXT,
-    hashed_key TEXT
+    name VARCHAR(255),
+    hashed_key VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE location_data (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     time DATETIME NOT NULL,
+    hashed_key VARCHAR(50),
     latitude DOUBLE NOT NULL,
     longitude DOUBLE NOT NULL,
     accuracy INTEGER,
     battery VARCHAR(50),
-    confidence INTEGER
+    confidence INTEGER,
+
+    CONSTRAINT tag_hash_foreign_key 
+        FOREIGN KEY (hashed_key) 
+        REFERENCES tags(hashed_key)
 );
--- insert_tag.py
+++ insert_tag.py
@@ -4,9 +4,10 @@ import sys
 import mysql.connector
 
 parser = argparse.ArgumentParser(add_help=True, description="Script used for inserting locations of a single tag into database. Expects data in json format.")
-parser.add_argument("--verbose", "-v", action ="store_true", help="Prints more information, does not print json. Used for manual testing.")
+parser.add_argument("--verbose", "-v", action ="store_true", help="Prints more information. Used for manual testing.")
 parser.add_argument("user", help="Mysql user.")
 parser.add_argument("password", help="Mysql user password.")
+parser.add_argument("tag_hash", help="Hashed public key of tag.")
 parser.add_argument("-host", default="localhost" ,help="Mysql host ip.")
 parser.add_argument("-database", default="tag_tracker", help="Name of database.")
 
@@ -26,7 +27,16 @@ my_database = mysql.connector.connect(
 cursor = my_database.cursor()
 
 def insert_entry(entry):
-    pass
+    sql = "INSERT INTO location_data (time, hashed_key, latitude, longitude, accuracy, battery, confidence) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    val = (entry["time"].replace("T"," "), args.tag_hash ,entry["latitude"], entry["longitude"], entry["accuracy"], entry["battery"], entry["confidence"])
+    try:
+        cursor.execute(sql,val)
+        my_database.commit()
+    except mysql.connector.IntegrityError:
+        print("Error: Tag is not yet registered")
+    except mysql.connector.Error as e:
+        print(f"Error: {e}")
+        
 
 stdin = sys.stdin.read().lstrip()
 decoder = json.JSONDecoder()
@@ -45,8 +55,11 @@ while len(stdin) > 0:
 if verbose:
     print(f"Read {len(json_found)} entries.")
     print("Inserting into database...")
+
+i = 0
 # Inserting each json object
 for entry in json_found:
-    time = entry.get("time")
-
-    print(time)
+    if verbose:
+        print(f"Inserting entry {i}")
+    insert_entry(entry)
+    i+=1
