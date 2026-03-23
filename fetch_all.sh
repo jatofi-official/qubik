fd0104e7326623936a70aa8058027526cbafc946
Jatofi <56402844+jatofi-official@users.noreply.github.com>
2026-03-23 17:45:40 +0100

    Update fetch_all.sh

--- fetch_all.sh
+++ fetch_all.sh
@@ -13,5 +13,5 @@ other_flags = ""
 
 
 while read -r arg1 arg2; do
-    python3 fetch_tag_locations.py "$arg1" "$arg2" | python3 insert_tag.py "$user" "$password" $other_flags
+    python3 fetch_tag_locations.py "$arg1" "$arg2" | python3 insert_tag.py "$user" "$password" "$arg1" $other_flags
 done < keys.txt
