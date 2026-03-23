c1979fe07af34c0326597c5449ca812f462003bb
Jatofi <56402844+jatofi-official@users.noreply.github.com>
2026-03-23 17:40:54 +0100

    Improved fetching script

--- fetch_all.sh
+++ fetch_all.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+{
+    read user
+    read password
+    read host
+    read database
+} < db.txt
+
+other_flags = ""
+[[ -n "$host" ]] && other_flags="$other_flags -host $host"   #I used AI for these two lines. They work by checking if variable has non-zero length, then it executes the second part after &&
+[[ -n "$database" ]] && other_flags="$other_flags -database $database"
+
+
 while read -r arg1 arg2; do
-    python3 fetch_tag_locations.py "$arg1" "$arg2"
+    python3 fetch_tag_locations.py "$arg1" "$arg2" | python3 insert_tag.py "$user" "$password" $other_flags
 done < keys.txt
